6a8891942ce942898788d56e696f246a78887e20
zenzen <aaaaru66@naver.com>
2023-07-26 19:49:33 +0900

    [아연] 초기 환경 구축 테스트 07-26 19:49

---
+++ src/main/java/com/example/PixelPro/repository/testRepository.java
@@ -0,0 +1,7 @@
+package com.example.PixelPro.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface testRepository extends JpaRepository {
+
+}
---
+++ src/main/java/com/example/PixelPro/service/testService.java
@@ -0,0 +1,9 @@
+package com.example.PixelPro.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class testService {
+}
