001c6ab7fdac9a9a80771f1f45e61d4f54b0c805
zenzen <aaaaru66@naver.com>
2023-07-26 18:39:44 +0900

    first commit

---
+++ src/main/java/com/example/PixelPro/PixelProApplication.java
@@ -0,0 +1,13 @@
+package com.example.PixelPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixelProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PixelPro/PixelProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PixelPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
