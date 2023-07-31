e4fad5ebc28a4544fa2e45aeda8f037c30dd874c
zenzen <aaaaru66@naver.com>
2023-07-31 23:10:25 +0900

    [아연] test 파일 삭제 07-31 23:10

--- src/main/java/com/example/PixelPro/controller/testController.java
+++
@@ -1,9 +0,0 @@
-package com.example.PixelPro.controller;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-
-@Controller
-@RequiredArgsConstructor
-public class testController {
-}
--- src/main/java/com/example/PixelPro/repository/testRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.PixelPro.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface testRepository{
-
-}
