73b1444435dd59df8e9830835243e3bbdf49a4bf
shinjuyoung <bo10820@naver.com>
2023-08-03 00:31:58 +0900

    [주영] Member, 개인퇴직금 추가 08-02 00:30

--- src/main/java/com/example/PixelPro/service/ResignService.java
+++ src/main/java/com/example/PixelPro/service/ResignService.java
@@ -1,9 +1,13 @@
 package com.example.PixelPro.service;
 
+import com.example.PixelPro.entity.ResignEntity;
+import com.example.PixelPro.entity.SalaryEntity;
 import com.example.PixelPro.repository.ResignRepository;
 import com.example.PixelPro.repository.SalaryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,4 +16,8 @@ public class ResignService {
     private final ResignRepository resignRepository;
 
 
+    public List<ResignEntity> findByOrderBySevpayDesc() {
+        List<ResignEntity> resign = resignRepository.findByOrderBySevpayDesc();
+        return resign;
+    }
 }
