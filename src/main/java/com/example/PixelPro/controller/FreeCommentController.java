133ec080b7956d39a67f5deb452fb697267c0aad
HannahSeol <tjfgkssk9472@gmail.com>
2023-08-08 19:09:12 +0900

    [한나] 익명게시판 mvc upload, header 수정, style.css 수정 08-08 19:07

---
+++ src/main/java/com/example/PixelPro/controller/FreeCommentController.java
@@ -0,0 +1,122 @@
+package com.example.PixelPro.controller;
+
+import com.example.PixelPro.Bean.FreeCommentBean;
+import com.example.PixelPro.entity.FreeCommentEntity;
+import com.example.PixelPro.entity.FreeEntity;
+import com.example.PixelPro.service.FreeCommentService;
+import com.example.PixelPro.service.FreeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class FreeCommentController {
+
+    private final FreeService freeService;
+    private final FreeCommentService freeCommentService;
+
+    //댓글 작성
+    @PostMapping(value = "community/free/writecomment")
+    public String writeFreeBoardComment(@RequestParam(value="fnum", required = false) int fnum,
+                                 @RequestParam(value="fcdetail", required = false) String fcdetail,
+                                 Model model){
+
+        FreeEntity freeEntity = freeService.findByFnum(fnum);
+
+        FreeCommentBean freeCommentBean = new FreeCommentBean();
+
+        freeCommentBean.setFnum(fnum);
+        freeCommentBean.setMbnum(freeEntity.getMbnum());
+        freeCommentBean.setFcdetail(fcdetail);
+
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        freeCommentBean.setFcdate(now.format(dateTimeFormatter));
+
+        List<FreeCommentEntity> commentlists = freeCommentService.getAllCommentLists(fnum);
+
+        if(commentlists.size() == 0 ) {
+            //첫 댓글의 fcstep = 0
+            freeCommentBean.setFcstep(0);
+        }
+        else {
+            //앞 댓글의 fcstep + 1
+            Integer beforeFcstep = freeCommentService.beforeFcstep(fnum);
+            freeCommentBean.setFcstep(beforeFcstep+1);
+        }
+
+        freeCommentBean.setFclevel(0);
+
+        FreeCommentEntity freeCommentEntity = FreeCommentEntity.insertFreeBoardComment(freeCommentBean);
+        freeCommentService.saveFreeComment(freeCommentEntity);
+
+        return "redirect:/community/free/detail?fnum=" + fnum;
+    }
+
+    //답글 작성
+    @PostMapping(value = "community/free/replycomment")
+    public String replyFreeBoardComment(@RequestParam(value = "fnum", required = false) int fnum,
+                                        @RequestParam(value = "fcnum", required = false) int fcnum,
+                                        @RequestParam(value = "fcdetail", required = false) String fcdetail,
+                                        Model model) {
+
+        FreeCommentEntity fcnumEntity = freeCommentService.findByFcnum(fcnum);
+
+        FreeCommentBean freeCommentBean = new FreeCommentBean();
+        freeCommentBean.setFnum(fnum);
+        freeCommentBean.setMbnum(fcnumEntity.getMbnum());
+        freeCommentBean.setFcdetail(fcdetail);
+        freeCommentBean.setFcstep(fcnumEntity.getFcstep());
+        freeCommentBean.setFclevel(fcnumEntity.getFclevel()+1);
+
+        Integer beforeFclevel = freeCommentService.beforeFclevel(fcnumEntity.getFcstep());
+
+        if(beforeFclevel == 0 ) {
+            //답글이 안달린 댓글의 fclevel = 0
+            freeCommentBean.setFclevel(fcnumEntity.getFclevel()+1);
+        }
+        else {
+            //댓글의 값이 가장 큰 fclevel + 1
+            freeCommentBean.setFclevel(beforeFclevel+1);
+        }
+
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        freeCommentBean.setFcdate(now.format(dateTimeFormatter));
+
+        FreeCommentEntity freeCommentEntity = FreeCommentEntity.insertFreeBoardComment(freeCommentBean);
+        freeCommentService.saveFreeComment(freeCommentEntity);
+
+        return "redirect:/community/free/detail?fnum=" + fnum;
+    }
+
+    //댓글 수정
+
+    //답글 수정
+
+    //댓글, 답글 삭제
+    @GetMapping(value = "community/free/deletecomment")
+    public String delete(@RequestParam("fcnum") int fcnum,
+                         @RequestParam("fnum") int fnum){
+
+        FreeCommentEntity fcnumEntity = freeCommentService.findByFcnum(fcnum);
+        if(fcnumEntity.getFclevel() == 0) {
+            fcnumEntity.setFcdetail("삭제된 댓글입니다.");
+            freeCommentService.saveFreeComment(fcnumEntity);
+        }
+        else {
+            freeCommentService.deleteByFcnum(fcnum);
+        }
+
+        return "redirect:/community/free/detail?fnum=" + fnum;
+    }
+
+}
---
+++ src/main/java/com/example/PixelPro/controller/FreeController.java
@@ -0,0 +1,163 @@
+package com.example.PixelPro.controller;
+
+import com.example.PixelPro.Bean.FreeBean;
+import com.example.PixelPro.entity.FreeCommentEntity;
+import com.example.PixelPro.entity.FreeEntity;
+import com.example.PixelPro.service.FreeCommentService;
+import com.example.PixelPro.service.FreeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequiredArgsConstructor
+public class FreeController {
+
+    /* 커뮤니티 - 익명게시판 */
+
+    private final FreeService freeService;
+    private final FreeCommentService freeCommentService;
+
+    //list 글 목록
+    @GetMapping(value = "community/free/list")
+    public String gotoFreeBoard(
+            @RequestParam(value="fcategory", required=false) String fcategory,
+            @RequestParam(value="keyword", required=false) String keyword,
+            @RequestParam(value="pageNumber", required=false) String pageNumber, Model model){
+
+        System.out.println(fcategory+"/"+keyword);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("fcategory", fcategory);
+        map.put("keyword", "%"+keyword+"%");
+
+        if(pageNumber == null) {
+            pageNumber = "1";
+        }
+
+        Page<FreeEntity> freeBoardList = freeService.getListsBySearch(map, Integer.valueOf(pageNumber)-1);
+        long totalCount = freeBoardList.getTotalElements();
+        long totalPages = freeBoardList.getTotalPages();
+
+        List<FreeEntity> lists = freeBoardList.getContent();
+
+        Map<Integer, Integer> comments = new HashMap<>();
+        for(FreeEntity freeEntity : lists) {
+            List<FreeCommentEntity> commentlists = freeCommentService.getAllCommentLists(freeEntity.getFnum());
+            comments.put(freeEntity.getFnum(), commentlists.size());
+        }
+
+        model.addAttribute("lists", lists);
+        model.addAttribute("comments", comments);
+        model.addAttribute("totalPages", totalPages);
+        model.addAttribute("totalCount", totalCount);
+
+        return "free/list";
+    }
+
+    //write 글 작성
+    @GetMapping(value = "community/free/write")
+    public String gotoWriteFreeBoard(Model model){
+        //아이디 정보 넘겨오기
+        //아이디 정보 보내기
+        model.addAttribute("freeBean", new FreeBean());
+        return "free/write";
+    }
+
+    @PostMapping(value = "community/free/write")
+    public String writeFreeBoard(@Valid FreeBean freeBean,
+                                 BindingResult bindingResult, Model model){
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("freeBean", freeBean);
+            return  "free/write";
+        }
+
+        //회원번호 임시 저장
+        freeBean.setMbnum(12345);
+
+        freeBean.setFcount(0);
+
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        freeBean.setFdate(now.format(dateTimeFormatter));
+
+        FreeEntity freeEntity = FreeEntity.insertFreeBoard(freeBean);
+        freeService.saveFree(freeEntity);
+
+        return "redirect:/community/free/list";
+    }
+
+    //detail 글 상세 내용
+    @GetMapping(value = "community/free/detail")
+    public String detailFreeBoard(Model model, @RequestParam("fnum") int fnum){
+        //회원번호 가져오기
+
+        //번호가 fnum인 data 가져오기
+        FreeEntity freeEntity = freeService.findByFnum(fnum);
+
+        //조회수 + 1
+        freeEntity.setFcount(freeEntity.getFcount()+1);
+        freeService.saveFree(freeEntity);
+
+        //댓글 전체 가져오기
+        List<FreeCommentEntity> commentlists = freeCommentService.getAllCommentLists(fnum);
+        System.out.println("commentlists.size:"+commentlists.size());
+
+        //원댓글 max(fcstep)
+        Integer beforeFcstep = freeCommentService.beforeFcstep(fnum);
+
+        model.addAttribute("free", freeEntity);
+        model.addAttribute("comment", commentlists);
+        model.addAttribute("maxfcstep", beforeFcstep);
+        return "free/detail";
+    }
+
+    //update 글 수정
+    @GetMapping(value = "community/free/update")
+    public String gotoUpdateFreeBoard(@RequestParam("fnum") int fnum, Model model){
+        //번호가 fnum인 data 가져오기
+        FreeEntity freeEntity = freeService.findByFnum(fnum);
+        model.addAttribute("freeBean", freeEntity);
+        return "free/update";
+    }
+
+    @PostMapping(value = "community/free/update")
+    public String updateFreeBoard(@Valid FreeBean freeBean,
+                                 BindingResult bindingResult, Model model){
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("freeBean", freeBean);
+            return  "free/update";
+        }
+
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        freeBean.setFdate(now.format(dateTimeFormatter));
+
+        FreeEntity freeEntity = FreeEntity.insertFreeBoard(freeBean);
+        freeService.saveFree(freeEntity);
+
+        return "redirect:/community/free/list";
+    }
+
+    //delete 글 삭제
+    @GetMapping(value = "community/free/delete")
+    public String delete(@RequestParam("fnum") int fnum){
+        freeService.deleteByFnum(fnum);
+        return "redirect:/community/free/list";
+    }
+
+}
