0d985131b1110a1df09ba90008945273f810e4f2
jscu0403 <jscu0403@gmail.com>
2023-08-08 16:50:12 +0900

    [김준수] 캘린더 초기설정 08-08 16:50

---
+++ src/main/java/com/example/PixelPro/repository/CalendarRepository.java
@@ -0,0 +1,8 @@
+package com.example.PixelPro.repository;
+
+import com.example.PixelPro.entity.Calendar;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CalendarRepository extends JpaRepository<Calendar, Integer> {
+
+}
